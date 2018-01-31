70ff69382589d8f81306217eb8d2e5d32916a758
Muhcim <34978284+Muhcim@users.noreply.github.com>
2018-02-01 01:44:48 +0300

    Bot Yapımı ve Kodları

---
+++ Bot Yapımı Güzel/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Bot_Yapımı_Güzel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtMesaj = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.LblHız = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Metin :";
+            // 
+            // TxtMesaj
+            // 
+            this.TxtMesaj.Location = new System.Drawing.Point(74, 12);
+            this.TxtMesaj.Multiline = true;
+            this.TxtMesaj.Name = "TxtMesaj";
+            this.TxtMesaj.Size = new System.Drawing.Size(356, 91);
+            this.TxtMesaj.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(202, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(350, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(129, 126);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(237, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hız:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tekrarlama";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(109, 280);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // LblHız
+            // 
+            this.LblHız.AutoSize = true;
+            this.LblHız.Location = new System.Drawing.Point(97, 158);
+            this.LblHız.Name = "LblHız";
+            this.LblHız.Size = new System.Drawing.Size(26, 13);
+            this.LblHız.TabIndex = 8;
+            this.LblHız.Text = "1ms";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 10;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 377);
+            this.Controls.Add(this.LblHız);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtMesaj);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtMesaj;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label LblHız;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Bot Yapımı Güzel/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bot_Yapımı_Güzel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dongu = 0;
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            LblHız.Text = trackBar1.Value.ToString() + "ms";//TrackBar'daki değer değiştiği an Label'imize yazsın dedik ama ilk önce Labelimizin üzerinde sağ tuş yaptık bring to front Seçeneğini tıkladık
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TxtMesaj.Text != "")
+            {
+                System.Threading.Thread.Sleep(3500);//Programı 3,5 saniye beklemeye alıcak
+                Timer.Interval = trackBar1.Value;//Timer'imizin  İntervalini TrackBar'a eşitliyoruz  hızını ayarlıyoruz
+                Timer.Enabled = true;//Timer Başlasın
+               // dongu = Convert.ToInt32(numericUpDown1.Value);//Tekrarlama değerini tutmak için oluşturduğumuz döngü değerini numericUpDown1'e eşitliyoruz
+
+                numericUpDown1.Enabled = false;//Değişken Barındıran bölümleri pasif hale getiriyoruz
+                TxtMesaj.Enabled = false;//Değişken Barındıran bölümleri pasif hale getiriyoruz
+                button1.Enabled = false;//Değişken Barındıran bölümleri pasif hale getiriyoruz
+                trackBar1.Enabled = false;//Değişken Barındıran bölümleri pasif hale getiriyoruz
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Metin Girişi Yapınız", "C# Bot", MessageBoxButtons.OK, MessageBoxIcon.Warning);//Metin kutusu Boş ise uyarı vermesi için mesaj ayarlıyoruz 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Timer.Enabled = false;//İşlemi iptal etmek için Timer'imizi durduruyoruz 
+            dongu = 0;//İşlem tamamlandığı için Timer'i sıfırlıyoruz
+
+            numericUpDown1.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+            TxtMesaj.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+            button1.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+            trackBar1.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+
+            MessageBox.Show("İşlem İptal Edildi", "C# Bot", MessageBoxButtons.OK, MessageBoxIcon.Warning);//Metin kutusu Boş ise uyarı vermesi için mesaj ayarlıyoruz 
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (dongu != Convert.ToInt32(numericUpDown1.Value))//Dinamik döngü değerini her seferinde kontrol ediyoruz ve tekrarlama değerinden küçük ise işlem gerçekleştiriyor
+            {
+                SendKeys.Send(TxtMesaj.Text + "\n");//SendKeys==Ekrana metni yazdırmak için kullandığımız method ve ("\n")=== Bir Alt satıra Geçmek için kullanılan komut
+                dongu += 1;//dongüyü tamamlamak için her seferinde 1 ekliyoruz taki tekrarlama eşit olana kadar 
+
+            }
+            if (dongu == Convert.ToInt32(numericUpDown1.Value))//döngü değeri sona erdiğinde çalıştırılacak komutlar için koşul oluşturuyoruz
+            {
+                Timer.Enabled = false;//Timer'imizi pasif yapıyoruz yani sonlandırıyoruz 
+                dongu = 0;//İşlem tamamlandığı için Timer'i sıfırlıyoruz
+
+                numericUpDown1.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+                TxtMesaj.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+                button1.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+                trackBar1.Enabled = true;//Değişken Barındıran bölümleri Aktif hale getiriyoruz
+
+                MessageBox.Show("İşlem Tamamlandı", "C# Bot", MessageBoxButtons.OK, MessageBoxIcon.Information);//Metin kutusu Boş ise uyarı vermesi için mesaj ayarlıyoruz 
+            }
+        }
+    }
+}
